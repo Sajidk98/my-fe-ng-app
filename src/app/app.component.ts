f8216861f1d6da2866004aa2650dfaa90be8ed99
sajidk98 <sajidansari18@gmail.com>
2025-05-18 19:25:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  template: `
+    <p>{{title}}<p>
+    <app-header></app-header>
+    <app-home />
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-fe-app';
+  title = 'Application Title';
 }
